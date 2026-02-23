6f4dd558a0d9cddc5d4441abf4d026b992ab97bd
mohanadqasim <mohanad.qasim@hotmail.com>
2026-02-23 03:23:34 +0300

    Test

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello World")
+    return jsonify(message="Hello World test")
 
 @app.route("/health")
 def health():
-    return jsonify(status="OK")
+    return jsonify(status="OK test")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
